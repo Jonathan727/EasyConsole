f58826473745e2ef7c6d32df8cbc30ee4821ea47
Jonathan Jackson <Jonathan+Git@jacksonengineering.net>
2022-05-09 15:42:51 -0500

    Add ActionMenuWithDefault

---
+++ EasyConsole/ActionMenuWithDefault.cs
@@ -0,0 +1,31 @@
+﻿namespace EasyConsole
+{
+    public class ActionMenuWithDefault : Menu
+    {
+        private readonly string _prompt;
+        private const int DefaultOptionIndex = 0;
+
+        private Option DefaultOption => Options[DefaultOptionIndex];
+
+        public ActionMenuWithDefault(string prompt, string defaultOption, Func<Task> @default) : this(prompt, new Option(defaultOption, @default))
+        {
+        }
+
+        public ActionMenuWithDefault(string prompt, Option defaultOption)
+        {
+            _prompt = prompt.Trim();
+            Add(defaultOption);
+            if (Options.Count != 1)
+            {
+                throw new InvalidOperationException($"Expected {nameof(Options)} to have only one option after adding the default");
+            }
+        }
+
+        protected override Option DisplayPrompt()
+        {
+            var choice = Input.ReadIntDoNotAppendDefaultToPrompt($"{_prompt}  [{DefaultOptionIndex + 1}. {DefaultOption.Name}]:", min: 1, max: Options.Count, DefaultOptionIndex + 1);
+            var chosenOption = Options[choice - 1];
+            return chosenOption;
+        }
+    }
+}
