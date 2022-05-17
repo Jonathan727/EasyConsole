01ef0406dc6089fa3073918c2672d34cee367f96
Jonathan Jackson <Jonathan+Git@jacksonengineering.net>
2022-05-17 16:19:21 -0500

    Revert "Drop non-default constructors from menu types and remove add methods"
    This reverts commit 6b2e6358f1f0518168ca7c16e863223024da2963.

--- EasyConsole/ValueMenu.cs
+++ EasyConsole/ValueMenu.cs
@@ -6,9 +6,29 @@
     /// <typeparam name="T"></typeparam>
     public class ValueMenu<T> : SingleChoiceMenuBase<T, ValueOption<T>, T>
     {
+        public ValueMenu()
+        {
+        }
+
+        public ValueMenu(string prompt, string defaultOption, T defaultValue) : this(prompt, new ValueOption<T>(defaultOption, defaultValue))
+        {
+        }
+
+        public ValueMenu(string prompt, ValueOption<T> defaultOption)
+        {
+            PromptText = prompt;
+            DefaultOption = defaultOption;
+        }
+
         protected override T OnUserAnsweredPrompt(ValueOption<T> selection)
         {
             return selection.Value;
+        }
+
+        public ValueMenu<T> Add(string name, T value)
+        {
+            Add(new ValueOption<T>(name, value));
+            return this;
         }
     }
 }
