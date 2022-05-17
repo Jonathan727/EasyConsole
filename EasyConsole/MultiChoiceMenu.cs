6b2e6358f1f0518168ca7c16e863223024da2963
Jonathan Jackson <Jonathan+Git@jacksonengineering.net>
2022-05-17 16:15:49 -0500

    Drop non-default constructors from menu types and remove add methods

--- EasyConsole/MultiChoiceMenu.cs
+++ EasyConsole/MultiChoiceMenu.cs
@@ -6,32 +6,9 @@
     /// <typeparam name="T"></typeparam>
     public class MultiChoiceMenu<T> : MultiChoiceMenuBase<T, ValueOption<T>, IReadOnlyCollection<T>>
     {
-        public MultiChoiceMenu()
-        {
-        }
-
-        public MultiChoiceMenu(string? prompt, string defaultOption, T defaultValue) : this(prompt, new ValueOption<T>(defaultOption, defaultValue))
-        {
-        }
-
-        public MultiChoiceMenu(string defaultOption, T defaultValue) : this(null, new ValueOption<T>(defaultOption, defaultValue))
-        {
-        }
-
-        public MultiChoiceMenu(string? prompt, ValueOption<T> defaultOption) : base(prompt)
-        {
-            DefaultOption = defaultOption;
-        }
-
         protected override IReadOnlyCollection<T> OnUserAnsweredPrompt(IEnumerable<ValueOption<T>> selection)
         {
             return selection.Select(x => x.Value).ToList();
-        }
-
-        public MultiChoiceMenu<T> Add(string name, T value)
-        {
-            Add(new ValueOption<T>(name, value));
-            return this;
         }
     }
 }
