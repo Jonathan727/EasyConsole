ae4cfe5b34dfb28482a4183139e4fe380a33f716
Jonathan Jackson <Jonathan+Git@jacksonengineering.net>
2022-05-26 11:36:37 -0500

    Run the selected tasks inside MultiChoiceActionMenu

--- EasyConsole/MultiChoiceActionMenu.cs
+++ EasyConsole/MultiChoiceActionMenu.cs
@@ -5,6 +5,10 @@
     /// </summary>
     public class MultiChoiceActionMenu : MultiChoiceMenuBase<Func<Task>, Option, Task>
     {
+        public CancellationToken CancellationToken { get; init; }
+        public int MaxConcurrentTasks { get; init; } = 10;
+        public TimeSpan DelayBeforeStartingEachTask { get; init; } = TimeSpan.Zero;
+
         public MultiChoiceActionMenu()
         {
         }
@@ -20,7 +24,43 @@
 
         protected override async Task OnUserAnsweredPrompt(IEnumerable<Option> selection)
         {
-            await Task.WhenAll(selection.Select(x => x.Value()));
+            if (MaxConcurrentTasks < 1)
+            {
+                throw new InvalidOperationException($"{nameof(MaxConcurrentTasks)} must be 1 or greater but was {MaxConcurrentTasks:N0}");
+            }
+
+            var tasksToRunNotStarted = selection.Select(x => x.Value).ToList();
+
+            var tasksRunning = new List<Task>();
+            while (tasksToRunNotStarted.Any() && tasksRunning.Count < MaxConcurrentTasks)
+            {
+                CancellationToken.ThrowIfCancellationRequested();
+                var taskToRun = tasksToRunNotStarted.First();
+                tasksRunning.Add(taskToRun.Invoke());
+                tasksToRunNotStarted.Remove(taskToRun);
+
+                await Task.Delay(DelayBeforeStartingEachTask, CancellationToken);
+            }
+            while (tasksRunning.Any())
+            {
+                Output.WriteLine($"Waiting for {tasksRunning.Count:N0} task(s) to finish. {tasksToRunNotStarted.Count:N0} task(s) queued but not started.");
+                var finishedTask = await Task.WhenAny(tasksRunning);
+                //await the finished task so that exceptions are thrown
+                await finishedTask;
+                tasksRunning.Remove(finishedTask);
+
+                //Start more tasks
+                while (tasksToRunNotStarted.Any() && tasksRunning.Count < MaxConcurrentTasks)
+                {
+                    CancellationToken.ThrowIfCancellationRequested();
+                    var taskToRun = tasksToRunNotStarted.First();
+                    tasksRunning.Add(taskToRun.Invoke());
+                    tasksToRunNotStarted.Remove(taskToRun);
+
+                    await Task.Delay(DelayBeforeStartingEachTask, CancellationToken);
+                }
+            }
+            await Task.WhenAll();
         }
 
         public void Add(string name, Func<Task> callback)
