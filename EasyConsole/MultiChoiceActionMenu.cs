a6c6d95ffe4c1e1ed2a3b2198944b164eb99c34e
Jonathan Jackson <Jonathan+Git@jacksonengineering.net>
2022-05-26 11:21:46 -0500

    Add new MultiChoiceActionMenu

---
+++ EasyConsole/MultiChoiceActionMenu.cs
@@ -0,0 +1,31 @@
+﻿namespace EasyConsole
+{
+    /// <summary>
+    /// Similar to <see cref="Menu"/>, but allows multiple choices
+    /// </summary>
+    public class MultiChoiceActionMenu : MultiChoiceMenuBase<Func<Task>, Option, Task>
+    {
+        public MultiChoiceActionMenu()
+        {
+        }
+
+        public MultiChoiceActionMenu(string? prompt, string defaultOption, Func<Task> defaultValue) : this(prompt, new Option(defaultOption, defaultValue))
+        {
+        }
+
+        public MultiChoiceActionMenu(string? prompt, Option defaultOption) : base(prompt)
+        {
+            DefaultOption = defaultOption;
+        }
+
+        protected override async Task OnUserAnsweredPrompt(IEnumerable<Option> selection)
+        {
+            await Task.WhenAll(selection.Select(x => x.Value()));
+        }
+
+        public void Add(string name, Func<Task> callback)
+        {
+            Add(new Option(name, callback));
+        }
+    }
+}
